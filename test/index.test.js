9d11b2dea7c199f58337f88667c3a203e901bafe
moyuxin <moyuxin@qiyou.cn>
2023-10-31 11:03:06 +0800

    chore: test optimization

--- test/index.test.js
+++ test/index.test.js
@@ -4,21 +4,49 @@ import postcss from 'postcss'
 
 suite('pxtorem Plugin Tests', () => {
   test('Should convert px to rem', async () => {
-    const code = `.container { width: 320px; font-size: 16px;}`
-    const processedCode = await postcss(pxtorem()).process(code)
+    const code = `.container {
+      width: 320px;
+      font-size: 16px;
+      .context {
+        height: 320px;
+        font-size: 32px
+      }
+    }`
+    const processedCode = postcss(pxtorem()).process(code).css
 
-    expect(processedCode.css).toMatch('.container { width: 320px; font-size: 1rem;}')
+    expect(processedCode).toMatch(`.container {
+      width: 320px;
+      font-size: 1rem;
+      .context {
+        height: 320px;
+        font-size: 2rem
+      }
+    }`)
   })
 
   test('Should handle pxtorem options', async () => {
-    const code = `.container { width: 320px; font-size: 16px;}`
+    const code = `.container {
+      width: 320px;
+      font-size: 16px;
+      .context {
+        height: 320px;
+        font-size: 32px
+      }
+    }`
     const options = {
       rootValue: 10,
       propList: ['*'],
     }
 
-    const processedCode = await postcss(pxtorem(options)).process(code)
+    const processedCode = postcss(pxtorem(options)).process(code).css
 
-    expect(processedCode.css).toMatch('.container { width: 32rem; font-size: 1.6rem;}')
+    expect(processedCode).toMatch(`.container {
+      width: 32rem;
+      font-size: 1.6rem;
+      .context {
+        height: 32rem;
+        font-size: 3.2rem
+      }
+    }`)
   })
 })
